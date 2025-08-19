cb87a975d5288b44026bc6da3c576617ada26b9a
Kang Sumin <neoramiregi@gmail.com>
2025-08-19 22:13:11 +0900

    chore: 초기 프로젝트 설정

---
+++ src/main/java/com/kt/api_analytics_svc/ApiAnalyticsSvcApplication.java
@@ -0,0 +1,13 @@
+package com.kt.api_analytics_svc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiAnalyticsSvcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiAnalyticsSvcApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kt/api_analytics_svc/ApiAnalyticsSvcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kt.api_analytics_svc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiAnalyticsSvcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
